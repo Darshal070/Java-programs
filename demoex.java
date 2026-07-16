9ba185c2973a79c9935e355760978a7f9e53aa4f
Darshal <dhadgedarshal@gmail.com>
2026-07-16 07:33:33 +0530

    Add files via upload

---
+++ demoex.java
@@ -0,0 +1,12 @@
+public class demoex {
+    String name="darshal";
+    static String name1="darshal";
+
+    public static void main(String args[]) {
+        int a=10;
+        demoex obj=new demoex();
+        System.out.println(a);
+        System.out.println(obj.name);
+        System.out.println(name1);
+    }
+}
