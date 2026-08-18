1c378db14c91551d6b9ece54ff37ef31c23fc066
Darshal <dhadgedarshal@gmail.com>
2026-08-18 11:35:08 +0530

    Add files via upload

---
+++ pr14_jumpingstmt.java
@@ -0,0 +1,22 @@
+class pr14_jumpingstmt {
+
+    public static void main(String[] args) {
+
+        //break stmt
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+
+        //continue stmt
+        for (int i = 1; i <= 10; i++) {
+            //    System.out.println(i);
+            if (i == 5) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+} 
