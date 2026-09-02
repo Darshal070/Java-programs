e13c2c5752fdcdff7fbcdc1d8df409d791418548
Darshal <dhadgedarshal@gmail.com>
2026-09-02 06:26:31 +0530

    Add files via upload

---
+++ pr34_Exception.java
@@ -0,0 +1,17 @@
+class pr34_Exception{
+    public static void main(String args[]){
+    try{
+        int a=10;
+        int b=0;
+        int c=a/b;
+        System.out.println(""+c);
+    }
+    catch(ArithmeticException e){
+        System.out.println(e);
+
+    }
+    finally{
+    System.out.println("Program executed successfully..!!");
+    }
+    }
+}
